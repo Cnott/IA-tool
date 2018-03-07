8d58565ade45b0dcac89bf60560fea097ca74b5b
Daniel Jigin <daniel.jigin@gmail.com>
2018-03-07 08:04:07 +0100

    Added CSVUTILS that can READ a .csv file and gives the data in a list
    CplxParsing should handle data concerning complexity

---
+++ src/parsing/CplxParsing.java
@@ -0,0 +1,41 @@
+package parsing;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileSystemView;
+
+import utils.CSVUtils;
+
+public class CplxParsing {
+
+	public CplxParsing() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) throws FileNotFoundException {
+
+		JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+
+		int returnValue = jfc.showOpenDialog(null);
+		// int returnValue = jfc.showSaveDialog(null);
+
+		if (returnValue == JFileChooser.APPROVE_OPTION) {
+			File selectedFile = jfc.getSelectedFile();
+			System.out.println(selectedFile.getAbsolutePath());
+
+			Scanner scanner = new Scanner(selectedFile);
+			while (scanner.hasNext()) {
+				List<String> line = CSVUtils.parseLine(scanner.nextLine());
+				System.out.println(
+						"Country [id= " + line.get(0) + ", code= " + line.get(1) + " , name=" + line.get(2) + "]");
+			}
+			scanner.close();
+		}
+
+	}
+
+}
