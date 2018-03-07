01ade1c66e501a1f24a28caf898862c5eb8fcb35
Cnott <deewewe99@hotmail.com>
2018-03-07 15:33:44 +0100

    Cleanedup

--- src/parsing/CplxParsing.java
+++ src/parsing/CplxParsing.java
@@ -13,29 +13,6 @@ public class CplxParsing {
 	}
 
 	public static void main(String[] args) throws FileNotFoundException {
-
-//		JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
-//
-//		int returnValue = jfc.showOpenDialog(null);
-//		// int returnValue = jfc.showSaveDialog(null);
-//
-//		if (returnValue == JFileChooser.APPROVE_OPTION) {
-//			File selectedFile = jfc.getSelectedFile();
-//			System.out.println(selectedFile.getAbsolutePath());
-//
-//			Scanner scanner = new Scanner(selectedFile);
-//			while (scanner.hasNext()) {
-//				List<String> line = Parser.parseCSCoupling("GitBridge.java", CSVUtils.parseLine(scanner.nextLine()));
-//				if (line != null) {
-//					for (String s : line) {
-//						System.out.print(s);
-//						System.out.print(",");
-//					}
-//					System.out.println();
-//				}
-//			}
-//			scanner.close();
-//		}
 		ArrayList<String> listToSearch=new ArrayList<String>();
 		listToSearch.add("GitBridge.java");
 		Parser.executeParse(listToSearch);
