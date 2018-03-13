4a2f94cb4c1e005b686aecfd99acbf96768a0d05
Cnott <deewewe99@hotmail.com>
2018-03-13 09:48:52 +0100

    Set a default size for the main frame

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -1,15 +1,16 @@
 package gui;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 import java.io.IOException;
 
 import javax.swing.JFrame;
-import javax.swing.JPanel;
 
 public class Gui {
 
 	public static void main(String[] args) throws IOException {
 		JFrame f=new JFrame();
+		f.setPreferredSize(new Dimension(1100,700));
 		f.add(new ImpactJTable(),BorderLayout.SOUTH);
 		f.setVisible(true);
 		f.add(new FileSystemPanel(), BorderLayout.WEST);
