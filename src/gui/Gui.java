26bc8bd2a919607037cba3d2c1d16ab99eb7f60d
Daniel Jigin <daniel.jigin@gmail.com>
2018-03-23 10:55:11 +0100

    Implemented remove feature

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -2,13 +2,19 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
 
 import utils.BinaryList;
 
@@ -20,11 +26,11 @@ public class Gui {
 		f.setPreferredSize(new Dimension(1100,700));
 		ImpactJTable impactTable = new ImpactJTable();
 		JPanel impactPanel = new JPanel();
-		//impactPanel.add(impactTable);
+		
 		impactPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 		JScrollPane scroll = new JScrollPane(impactTable);
 		scroll.setPreferredSize(new Dimension(1100, 200));
-	//	scroll.set
+
 		impactPanel.add(scroll);
 		f.add(impactPanel, BorderLayout.SOUTH);
 		
@@ -33,6 +39,7 @@ public class Gui {
 		leftTopPanel.setLayout(new BoxLayout(leftTopPanel,BoxLayout.Y_AXIS));
 		leftTopPanel.add(new FileSystemPanel(impactSet),BorderLayout.CENTER);
 		leftTopPanel.add(new DependencyButton(impactSet,impactTable));
+		
 		
 		f.add(leftTopPanel, BorderLayout.WEST);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
--- src/gui/ImpactJTable.java
+++ src/gui/ImpactJTable.java
@@ -1,12 +1,14 @@
 package gui;
 
-import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.util.List;
 import java.util.Vector;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.JLabel;
-import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
@@ -15,51 +17,81 @@ import org.apache.commons.csv.CSVRecord;
 
 import utils.CSVUtils;
 
-
 public class ImpactJTable extends JTable {
-		private static final String[] COL_NAMES = { "File","Coupled File","Degree of Coupling","Cyclomatic Complexity"
-				 									,"Test Coverage","Sum of Coupling","Long Method","Many Parameters\r\n"};
-		
-		public ImpactJTable() throws IOException {
-			DefaultTableModel model=new DefaultTableModel(COL_NAMES,0);
-			List<CSVRecord> list=CSVUtils.readResFile();
-	        for( CSVRecord row : list ) {
-	        	Vector<String> rowToAppend=new Vector<String>();
-	            for( String entry : row ) {
-	                rowToAppend.add(entry);
-	            }
-	            model.addRow(rowToAppend);
-	        }
-	        setModel(model);
-	        centerText();
-	        //Dont know if this is needed
-	        //setPreferredSize(new Dimension(1100, 150));
-	        setVisible(true);
-		}
-		
-		private void centerText() {
-			DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
-			centerRenderer.setHorizontalAlignment( JLabel.LEFT );
-			for(int i = 0; i<getColumnCount(); i++) {
-				getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
-			}
-		}
+	private static final String[] COL_NAMES = { "File", "Coupled File", "Degree of Coupling", "Cyclomatic Complexity",
+			"Test Coverage", "Sum of Coupling", "Long Method", "Many Parameters\r\n" };
 
-		public void update() throws IOException {
-			// TODO Auto-generated method stub
-			
-			DefaultTableModel model=new DefaultTableModel(COL_NAMES,0);
-			List<CSVRecord> list=CSVUtils.readResFile();
-	        for( CSVRecord row : list ) {
-	        	Vector<String> rowToAppend=new Vector<String>();
-	            for( String entry : row ) {
-	                rowToAppend.add(entry);
-	            }
-	            model.addRow(rowToAppend);
-	        }
-	        setModel(model);
-	        centerText();
-			
+	private DefaultTableModel model;
+
+	public ImpactJTable() throws IOException {
+		model = new DefaultTableModel(COL_NAMES, 0);
+		List<CSVRecord> list = CSVUtils.readResFile();
+		for (CSVRecord row : list) {
+			Vector<String> rowToAppend = new Vector<String>();
+			for (String entry : row) {
+				rowToAppend.add(entry);
+			}
+			model.addRow(rowToAppend);
 		}
+		model.addColumn("Remove");
+		setModel(model);
+		centerText();
+		// Dont know if this is needed
+		// setPreferredSize(new Dimension(1100, 150));
+
+		Action delete = new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				ImpactJTable table = (ImpactJTable) e.getSource();
+				int modelRow = Integer.valueOf(e.getActionCommand());
+				model.removeRow(modelRow);
+			}
+		};
+
+		ButtonColumn buttonColumn = new ButtonColumn(this, delete, model.getColumnCount() - 1);
+		buttonColumn.setMnemonic(KeyEvent.VK_D);
+
+		setVisible(true);
+	}
+
+	private void centerText() {
+		DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
+		centerRenderer.setHorizontalAlignment(JLabel.LEFT);
+		for (int i = 0; i < getColumnCount(); i++) {
+			getColumnModel().getColumn(i).setCellRenderer(centerRenderer);
+		}
+	}
+
+	public void update() throws IOException {
+		// TODO Auto-generated method stub
+		System.out.println("hej");
+		model = new DefaultTableModel(COL_NAMES, 0);
+		List<CSVRecord> list = CSVUtils.readResFile();
+		for (CSVRecord row : list) {
+			Vector<String> rowToAppend = new Vector<String>();
+			for (String entry : row) {
+				rowToAppend.add(entry);
+			}
+			model.addRow(rowToAppend);
+		}
+		model.addColumn("Remove");
+		setModel(model);
+		centerText();
+		Action delete = new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				ImpactJTable table = (ImpactJTable) e.getSource();
+				int modelRow = Integer.valueOf(e.getActionCommand());
+				model.removeRow(modelRow);
+			}
+		};
+
+		ButtonColumn buttonColumn = new ButtonColumn(this, delete, model.getColumnCount() - 1);
+		buttonColumn.setMnemonic(KeyEvent.VK_D);
 		
+
+	}
+
+	public DefaultTableModel getModel() {
+		return model;
+	}
+
 }
