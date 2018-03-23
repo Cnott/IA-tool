fadf62baba02640d770462be57d537e61ed703ef
Daniel Jigin <daniel.jigin@gmail.com>
2018-03-23 11:06:18 +0100

    Adjusted window size

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -2,19 +2,14 @@ package gui;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
-import java.awt.event.ActionEvent;
-import java.awt.event.KeyEvent;
+import java.awt.Toolkit;
 import java.io.IOException;
 
-import javax.swing.AbstractAction;
-import javax.swing.Action;
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
-import javax.swing.JTable;
-import javax.swing.table.DefaultTableModel;
 
 import utils.BinaryList;
 
@@ -22,14 +17,14 @@ public class Gui {
 	
 	public static void main(String[] args) throws IOException {
 		JFrame f=new JFrame("Impact Analysis and parameters indicating Technical Debt");
-		f.setLocation(150,150);
-		f.setPreferredSize(new Dimension(1100,700));
+		f.setPreferredSize(new Dimension(1300,750));
+		
 		ImpactJTable impactTable = new ImpactJTable();
 		JPanel impactPanel = new JPanel();
 		
 		impactPanel.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
 		JScrollPane scroll = new JScrollPane(impactTable);
-		scroll.setPreferredSize(new Dimension(1100, 200));
+		scroll.setPreferredSize(new Dimension(1300, 250));
 
 		impactPanel.add(scroll);
 		f.add(impactPanel, BorderLayout.SOUTH);
@@ -39,12 +34,13 @@ public class Gui {
 		leftTopPanel.setLayout(new BoxLayout(leftTopPanel,BoxLayout.Y_AXIS));
 		leftTopPanel.add(new FileSystemPanel(impactSet),BorderLayout.CENTER);
 		leftTopPanel.add(new DependencyButton(impactSet,impactTable));
-		
+				
 		
 		f.add(leftTopPanel, BorderLayout.WEST);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		f.pack();
+		f.setLocationRelativeTo(null);
 		f.setVisible(true);
 	}
 
